61082266b063bd8305fd574ed2b528436e9ee973
David <1697252093@qq.com>
2023-08-26 18:53:39 +0800

    the little bug

--- backend/faceback/models.py
+++ backend/faceback/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 import uuid
 from django.core.files.storage import FileSystemStorage
-from backend.settings import MEDIA_ROOT, MEDIA_URL
+from back.settings import MEDIA_ROOT, MEDIA_URL
 
 
 # Create your models here.
